b2b4f2c38f3afc2b6101dd6586c9caccfa27fb4f
김순홍 <shong1332@naver.com>
2025-03-18 01:34:05 +0900

    [init]   - shongram 프로잭트 추가

---
+++ src/main/java/com/shong/shongram/ShonGramApplication.java
@@ -0,0 +1,13 @@
+package com.shong.shongram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShonGramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShonGramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shong/shongram/ShonGramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shong.shongram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShonGramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
